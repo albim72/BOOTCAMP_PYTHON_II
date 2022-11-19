a84db7e974be8dbe917dd9f07d10826cd2d6df3e
albim72 <marcin.albiniak72@gmail.com>
2022-11-19 14:55:49 +0100

    Create zbiory.py

---
+++ DZIEN_1/zbiory.py
@@ -0,0 +1,18 @@
+drzewa = {"buk","jodła","dąb","jesion","baobab","jabłoń","buk"}
+print(drzewa)
+print(drzewa)
+print(drzewa)
+
+for d in drzewa:
+    print(d)
+
+print("osika" in drzewa)
+drzewa.add("osika")
+print(drzewa)
+
+las = ["świerk","topola","brzoza","olszyna"]
+drzewa.update(las)
+
+print(drzewa)
+
+liczby = [34,0,3,4,1,3,4,5,67,8,9,2,3,4,3,8,9,67,7,8,2,3,1,4,5,1,2,3,4]

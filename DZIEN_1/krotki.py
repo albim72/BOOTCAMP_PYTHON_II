65f11ec9057555413b266e044d7e178e4b66bba8
albim72 <marcin.albiniak72@gmail.com>
2022-11-19 14:04:00 +0100

    Update krotki.py

--- DZIEN_1/krotki.py
+++ DZIEN_1/krotki.py
@@ -3,3 +3,25 @@ print(animal)
 print(type(animal))
 print(animal[5])
 print(animal[2:4])
+
+
+if "papuga" in animal:
+    print("tak! papuga to zwierz...")
+
+if "budynek" in animal:
+    print("to jest błąd!")
+
+print("pies" in animal)
+anim2 = ("pająk","mucha")
+
+animal = animal + anim2
+
+print(animal)
+
+mojakrotka = tuple(("obiekt56",67,89,10.05545,True,"Urszula"))
+print(mojakrotka)
+
+#zmodyfikuj krotkę: usuń wartość 89, zamień imię "Urszula" na "Karol"
+#wstaw na pozycji 4 wartość 100, wastaw na końcu listy False
+
+

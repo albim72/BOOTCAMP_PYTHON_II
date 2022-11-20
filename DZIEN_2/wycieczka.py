20a9d5caae25fbff1598978dc855e3353712d64c
albim72 <marcin.albiniak72@gmail.com>
2022-11-20 13:09:23 +0100

    Update wycieczka.py

--- DZIEN_2/wycieczka.py
+++ DZIEN_2/wycieczka.py
@@ -2,8 +2,13 @@
 #--> (transport+nocleg_wyzywienie)*(1-rabat)+wycieczki+ubezpieczenie+inne
 
 def kwota(t,nw,w,u,i,rab=0):
-    return (nw+t)*(1-rab/100)+w+u+i
+    if rab >= 0 and rab <= 35:
+        return (nw + t) * (1 - rab / 100) + w + u + i
+    else:
+        return "podaj rabat mniejszy niż 36% lub większy od 0"
 
 print("kwota do zapłaty",kwota(100,100,50,50,50),"zł")
 print("kwota do zapłaty",kwota(100,100,50,50,50,25),"zł")
 print("kwota do zapłaty",kwota(100,100,50,50,50,75),"zł")
+
+#przbuduj funkcję kwota tak aby przyjmowała tylko rabat w zakresie <1,35>

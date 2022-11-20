141a17647b85dd07469ceeed5367f10bb5e5935e
albim72 <marcin.albiniak72@gmail.com>
2022-11-20 13:21:31 +0100

    Update wycieczka.py

--- DZIEN_2/wycieczka.py
+++ DZIEN_2/wycieczka.py
@@ -12,3 +12,25 @@ print("kwota do zapłaty",kwota(100,100,50,50,50,25),"zł")
 print("kwota do zapłaty",kwota(100,100,50,50,50,75),"zł")
 
 #przbuduj funkcję kwota tak aby przyjmowała tylko rabat w zakresie <1,35>
+
+
+#dla rabatu 0 wypisz -> kwota bazowa wynosi: kwota
+#dla rabatów pozostałych -> dla rabatu x% kwota do zapłaty wynosi: kwota
+rabs = [0,8,10,12,15,20,25]
+#dodatkowo: zapisz kwoty które policzyliśmy dla każdego rabatu do tablicy wynik =[]
+print("_____________________________________________________")
+wynik = []
+for r in rabs:
+    kw = kwota (1700, 1230, 450, 200, 180, r)
+
+    if r == 0:
+        print (f"cena bazowa do zapłaty wynosi: {kw:.2f} zł")
+    else:
+        print (f"dla rabatu: {r} %, kwota do zapłaty wynosi: {kw:.2f} zł")
+
+    wynik.append(kw)
+
+print(f"tablica wyników/append/: {wynik}")
+
+result = [kwota(1700,1230,450,200,180,r) for r in rabs]
+print(f"tablica wyników/list comprehension/: {result}")

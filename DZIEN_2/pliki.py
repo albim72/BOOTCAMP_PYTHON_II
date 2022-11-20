9e94578b3a0bebc9652c76622d25a50e011eda3b
albim72 <marcin.albiniak72@gmail.com>
2022-11-20 14:02:03 +0100

    Create pliki.py

---
+++ DZIEN_2/pliki.py
@@ -0,0 +1,30 @@
+import os
+
+print("_________ czytanie zawartości pliku za pomocą readline() ____________")
+f = open("dane.txt","r",encoding="utf-8")
+print(f.readline(),end="")
+print(f.readline(),end="")
+print(f.readline(),end="")
+print(f.readline())
+f.close()
+
+print("\n_________ czytanie zawartości pliku za pomocą read() ____________")
+f = open("dane.txt","r",encoding="utf-8")
+print(f.read())
+f.close()
+
+print("\n_________ czytanie zawartości pliku za pomocą pętli for ____________")
+f = open("dane.txt","r",encoding="utf-8")
+for linia in f:
+    print(linia, end="")
+f.close()
+
+print("\n_________ tworzenie nowego pliku ____________")
+f = open("message.txt","a",encoding="utf-8")
+f.write("to jest ważna informacja\n")
+f.close()
+
+f = open("message.txt","r",encoding="utf-8")
+print(f.read())
+f.close()
+

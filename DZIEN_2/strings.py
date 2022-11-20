9a1b32f44f7dacc99ef9765582471046ccdcb2f9
albim72 <marcin.albiniak72@gmail.com>
2022-11-20 09:11:03 +0100

    Create strings.py

---
+++ DZIEN_2/strings.py
@@ -0,0 +1,14 @@
+a=45
+b=7
+print("To jest wartość a = ", end=" ")
+print(a,end="")
+print(", to jest druga wartość b =",end=" ")
+print(b)
+
+mecz = "mecz miesiąca\nklub sportowy: \"Orły Wisła\" - \tTrener: Jan Kot\nlokalizacja: Janowo\n" \
+       "vs\nklub sportowy: \"Strome Wzgórza KS\" - \tTrener: Adam Nowak\nlokalizacja: Strome Wzgórza\n"
+
+print(mecz)
+
+str_ = "        niezwykle ważna i Tajna wiadomość; ERT6556566454;        zaszyfrowana przez Aden Knot;" \
+       "i równie Tajna pRzEsYłKA"

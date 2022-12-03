8f652b0fe5bb720d842fbbdeba5a89a17128952c
albim72 <marcin.albiniak72@gmail.com>
2022-12-03 14:42:23 +0100

    Update pochodne.py

--- DZIEN_3/pochodne.py
+++ DZIEN_3/pochodne.py
@@ -1,25 +1,28 @@
 class Base(object):
     def __init__(self,name):
         self.name = name
-        
+
     def getname(self):
         return self.name
-    
-    
+
+
 class Child(Base):
     def __init__(self,name,age):
         super().__init__(name)
         self.age = age
-        
+
     def getage(self):
         return self.age
-    
+
 class GrandChild(Child):
     def __init__(self,name,age,city):
         super().__init__(name,age)
         self.city = city
-        
+
     def getcity(self):
         return self.city
-        
-        
+
+
+osoba = GrandChild("Bonifacy",19,"Kraków")
+print(f'student: {osoba.getname()}, wiek: {osoba.getage()}, miasto: {osoba.getcity()}.')
+

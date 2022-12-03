16a5439da84c816f154ef24b2603a26711b85e28
albim72 <marcin.albiniak72@gmail.com>
2022-12-03 15:55:56 +0100

    Update kolo.py

--- DZIEN_3/FIGURY/kolo.py
+++ DZIEN_3/FIGURY/kolo.py
@@ -1,5 +1,14 @@
+import math
+
 from figura import Figura
 
-#utwórz klasę Kolo(Figura) i wyznacz pole koła oraz opisz konstruktor, a następnie w main() policz pole koła dla 
+#utwórz klasę Kolo(Figura) i wyznacz pole koła oraz opisz konstruktor, a następnie w main() policz pole koła dla
 #promienia = 5.5
 #polekola = pi*a**2 -> math.pi
+
+class Kolo(Figura):
+    def __init__(self,a):
+        super().__init__(a,0)
+
+    def policz_pole(self):
+        return math.pi*self.a**2

7a8845089da778d840ae29d4cb0551bc24229620
albim72 <marcin.albiniak72@gmail.com>
2022-12-04 09:03:35 +0100

    Update pojazd.py

--- DZIEN_4/POJAZD_SPALANIE/pojazd.py
+++ DZIEN_4/POJAZD_SPALANIE/pojazd.py
@@ -1,8 +1,22 @@
 from IPojazd import IPojazd
+from IDane import IDane
 
-class Pojazd(IPojazd):
+class Pojazd(IPojazd,IDane):
+    def __init__(self,marka,model,rocznik,cena):
+        self.marka = marka
+        self.model = model
+        self.rocznik = rocznik
+        self.cena = cena
+
     def spalanie_100(self, odl, litry):
         return litry*100/odl
 
     def koszt_przejazdu(self, odl, litry, cena_l):
         return self.spalanie_100(odl,litry)*(odl/100)*cena_l
+
+    def opispojazdu(self):
+        return f"Pojazd marki {self.marka} model {self.model}, rocznik: {self.rocznik}, cena: {self.cena} zł"
+
+    def silnik(self, rodzaj, poj, l_km):
+        return f"silnik {rodzaj}, pojemność: {poj},liczba przejechanych kilometrów: {l_km}"
+

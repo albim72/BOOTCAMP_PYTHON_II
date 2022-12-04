e48f1576f76115c6cb2712b9a84d93e0c8049494
albim72 <marcin.albiniak72@gmail.com>
2022-12-04 13:52:03 +0100

    Create main.py

---
+++ DZIEN_4/DICT_ERROR/main.py
@@ -0,0 +1,3 @@
+#Tworzenie słownika opartego na dwóch kolekcjach (lista,krotka) wartości mogą przyjmować tylko typy: 
+#float i int, pozostałem zachowanie mają być traktowane jako błąd
+#napisz dwie klasy błędu: jedna do ograniczeń kolekcji , druga do ograniczeń typów danych w wartościch słowników

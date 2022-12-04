66c0626e49a71e2689036de883499d205b19e2c2
albim72 <marcin.albiniak72@gmail.com>
2022-12-04 15:19:15 +0100

    Update silnia.py

--- DZIEN_4/SILNIA_z_BLEDEM/silnia.py
+++ DZIEN_4/SILNIA_z_BLEDEM/silnia.py
@@ -2,12 +2,13 @@
 #doble -> 1.8E308
 #????n -> 171!
 import sys
+from dziedzinasilnia import DziedzinaSilnia
 sys.set_int_max_str_digits(1000000000)
 sys.setrecursionlimit(0x10000000)
 
 def silnia(n):
     if n<0:
-        raise ValueError("silnia niezdefiniowana dla liczb ujmenych!")
+        raise DziedzinaSilnia(n)
     wynik = 1
     for i in range(1,n+1):
         wynik*=i
@@ -15,7 +16,7 @@ def silnia(n):
 
 def silnia_rek(n):
     if n<0:
-        raise ValueError("silnia niezdefiniowana dla liczb ujmenych!")
+        raise DziedzinaSilnia(n)
     if n == 0:
         return 1
     else:
@@ -25,5 +26,8 @@ try:
     n = int(input("podaj wartość argumentu n funkcji silnia: "))
     print(f"silnia z n={n} wynosi: {silnia(n)}")
     print(f"silnia rekurencyjna z n={n} wynosi: {silnia_rek(n)}")
-except ValueError as ve:
+except DziedzinaSilnia as ve:
     print(ve)
+
+#napisz klasę błędu DziedzinaSilnia. która będzie reagować na zadanie wartości ujemnych argumentu n
+#wepnij klasę błędu w rozwiązanie

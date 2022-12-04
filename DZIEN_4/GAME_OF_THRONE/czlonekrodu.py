3d7a0813970a09aa402b1c3d985706947f5a6097
albim72 <marcin.albiniak72@gmail.com>
2022-12-04 10:12:54 +0100

    Update czlonekrodu.py

--- DZIEN_4/GAME_OF_THRONE/czlonekrodu.py
+++ DZIEN_4/GAME_OF_THRONE/czlonekrodu.py
@@ -1,21 +1,25 @@
 from abc import ABC,abstractmethod
 
 class CzlonekRodu(ABC):
-    def __init__(self,nazwa_rodu,plec,punkty_walki,punkty_palacowe,doswiadczenie):
+    def __init__(self,nazwa_rodu,tytul,plec,punkty_walki,punkty_palacowe,doswiadczenie):
+        self.tytul = tytul
         self.nazwa_rodu = nazwa_rodu
         self.plec = plec
         self.punkty_walki = punkty_walki
         self.punkty_palacowe = punkty_palacowe
         self.doswiadczenie = doswiadczenie
-        
+
     @abstractmethod
     def walka(self):
         pass
-    
+
     @abstractmethod
     def negocjacja(self):
         pass
-    
+
     @abstractmethod
     def uczta(self):
         pass
+
+    def get_tytul(self):
+        return self.tytul

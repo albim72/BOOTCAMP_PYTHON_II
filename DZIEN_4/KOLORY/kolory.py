5c1a314fd97287a6ed258efffe794065e9236dbd
albim72 <marcin.albiniak72@gmail.com>
2022-12-04 11:17:10 +0100

    Create kolory.py

---
+++ DZIEN_4/KOLORY/kolory.py
@@ -0,0 +1,3 @@
+#stwórz klasę Kolory (moduł), konstruktor (id,nazwa_koloru,paleta)
+#dla pól id,nazwa_koloru,paleta stwórz gettery
+#w module głównym stwórz instancję klasy Kolory i wyświetl getterami wartości: nazwa_koloru,paleta

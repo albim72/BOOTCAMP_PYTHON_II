890060876084ce1776f4cbb4f3a00288094121a5
albim72 <marcin.albiniak72@gmail.com>
2022-12-04 14:07:36 +0100

    Create dictionary.py

---
+++ DZIEN_4/DICT_ERROR/dictionary.py
@@ -0,0 +1,9 @@
+from typedataerror import IntFloatValueEror
+from dict_model import KeyValueConstructError
+
+
+class CustomInFloatDict(dict):
+    
+    empty_dict ={}
+    def __init__(self,key=None,value=None):
+        pass

8c4a993b4ef9241874ad33d57b3bb6986ed9f20e
albim72 <marcin.albiniak72@gmail.com>
2022-12-04 13:37:45 +0100

    Update filemanager.py

--- DZIEN_4/filemanager.py
+++ DZIEN_4/filemanager.py
@@ -7,10 +7,19 @@ class FileManager:
         self.mode = mode
         self.encod = encod
         self.file = None
-        
+
     def __enter__(self):
         self.file = open(self.filename,self.mode,encoding=self.encod)
         return self.file
-    
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.file.close()
+
+with FileManager('test.txt','a','utf-8') as f:
+    f.write('to jest bardzo ważny test\n')
+
+print(f.closed)
+
+with FileManager('test.txt','r','utf-8') as f:
+    print(f.read())
+

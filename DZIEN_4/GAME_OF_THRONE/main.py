455f9a7ef875d7a851961a567adfe3ddad5e80a6
albim72 <marcin.albiniak72@gmail.com>
2022-12-04 10:13:15 +0100

    Update main.py

--- DZIEN_4/GAME_OF_THRONE/main.py
+++ DZIEN_4/GAME_OF_THRONE/main.py
@@ -2,21 +2,21 @@ from tywin import Tywin
 from tyrion import Tyrion
 from cersei import Cersei
 
-tyw = Tywin("Lannister","m",8,6,9)
-tyr = Tyrion("Lannister","m",6,9,10)
-cer = Cersei("Lannister","k",4,9,9)
+tyw = Tywin("Lannister","Lord","m",8,6,9)
+tyr = Tyrion("Lannister","Lord","m",6,9,10)
+cer = Cersei("Lannister","Królowa","k",4,9,9)
 
-print("___________ lord Tywin ____________")
+print(f"___________ {tyw.get_tytul()} {tyw.__class__.__name__} ____________")
 tyw.walka()
 tyw.negocjacja()
 tyw.uczta()
 
-print("___________ lord Tyrion ____________")
+print(f"___________ {tyr.get_tytul()} {tyr.__class__.__name__} ____________")
 tyr.walka()
 tyr.negocjacja()
 tyr.uczta()
 
-print("___________ królowa Cerei ____________")
+print(f"___________ {cer.get_tytul()} {cer.__class__.__name__} ____________")
 cer.walka()
 cer.negocjacja()
 cer.uczta()

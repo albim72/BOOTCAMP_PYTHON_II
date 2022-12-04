d4786738002c4168a8f2ef28e5e1d547bb27aef1
albim72 <marcin.albiniak72@gmail.com>
2022-12-04 11:41:08 +0100

    Update main.py

--- DZIEN_4/KOLORY/main.py
+++ DZIEN_4/KOLORY/main.py
@@ -1,3 +1,10 @@
 #stwórz klasę Kolory (moduł), konstruktor (id,nazwa_koloru,paleta)
 #dla pól id,nazwa_koloru,paleta stwórz gettery
 #w module głównym stwórz instancję klasy Kolory i wyświetl getterami wartości: nazwa_koloru,paleta
+
+from kolory import Kolor
+
+kol = Kolor(45,"czerwony","ABC")
+print(f"id: {kol.id}")
+print(f"kolor: {kol.kolor}")
+print(f"paleta: {kol.paleta}")

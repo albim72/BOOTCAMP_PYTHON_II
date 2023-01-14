b466d99972eeaa245cc23dbe930dabb2de1e7793
albim72 <marcin.albiniak72@gmail.com>
2023-01-14 11:24:17 +0100

    Create simplebucket.py

---
+++ DZIEN_5/BUCKET_PROJ/simplebucket.py
@@ -0,0 +1,10 @@
+from datetime import datetime,timedelta
+
+class Bucket:
+    def __init__(self,period):
+        self.period_delta = timedelta(seconds=period)
+        self.reset_time = datetime.now()
+        self.quota = 0
+        
+    def __repr__(self):
+        return f'Bucket(quota={self.quota})'

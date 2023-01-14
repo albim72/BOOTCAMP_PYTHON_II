65a279cea8183471ba871d77c7eec63cc2187d71
albim72 <marcin.albiniak72@gmail.com>
2023-01-14 10:37:28 +0100

    Update dt_article.py

--- DZIEN_5/dt_article.py
+++ DZIEN_5/dt_article.py
@@ -17,19 +17,24 @@ class PythonArticle(Article):
     language:str
     author:str
     price:int = 0
-    
+
     def __repr__(self):
         return f'{self.language} -- {self.author} -- {self.price} zł'
-    
+
     def __init__(self,title,price):
         self.title = title
         self.price = price
         self.language = "Python 3"
         self.author = "Marcin Albiniak"
         self.content = "opis wybranych aspektów użycia języka Python"
-        
+
     def infoarticle(self):
         return f'Publikacja: {self.title}, autor: {self.author}, rabat: {0.1*self.price} zł, ' \
                f'cena do zapłaty: {0.9*self.price} zł'
-        
-        
+
+
+artpy = PythonArticle("Algorytmy DL",98)
+print(artpy)
+print(artpy.infoarticle())
+
+#artpy2 = PythonArticle("Python","Adam",54)

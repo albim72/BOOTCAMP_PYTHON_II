e280ee80e0f98c6adf8dc1fe9945926f1c44c53a
albim72 <marcin.albiniak72@gmail.com>
2023-01-14 13:16:00 +0100

    Update main.py

--- DZIEN_5/EGZAMINY/main.py
+++ DZIEN_5/EGZAMINY/main.py
@@ -1,6 +1,14 @@
 from homework import Homework
+from exam import Exam
 
 jurek = Homework()
-jurek.grade = 97
+jurek.grade = 94
 
 assert jurek.grade == 94
+
+st = Exam()
+st.writing_grade = 80
+st.math_grade = 76
+
+assert st.writing_grade == 80
+assert st.math_grade == 76

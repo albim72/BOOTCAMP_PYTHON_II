d36846048d006ae34a429e8113ffd43fe6d914a8
albim72 <marcin.albiniak72@gmail.com>
2023-01-14 13:24:22 +0100

    Update main.py

--- DZIEN_5/EGZAMINY/main.py
+++ DZIEN_5/EGZAMINY/main.py
@@ -1,14 +1,35 @@
 from homework import Homework
-from exam import Exam
+from exam import OldExam
+from grade import Exam
 
 jurek = Homework()
 jurek.grade = 94
 
 assert jurek.grade == 94
 
-st = Exam()
+st = OldExam()
 st.writing_grade = 80
 st.math_grade = 76
 
 assert st.writing_grade == 80
 assert st.math_grade == 76
+
+print("__________egzaminy: pierwszy i drugi________")
+
+first_exam = Exam()
+first_exam.writing_grade = 82
+first_exam.science_grade = 76
+first_exam.math_grade = 56
+
+print("___________egazmin pierwszy________")
+print(f'Pisanie -> ocena: {first_exam.writing_grade}')
+print(f'Nauka -> ocena: {first_exam.science_grade}')
+print(f'Matematyka -> ocena: {first_exam.math_grade}')
+
+print("___________egazmin drugi________")
+second_exam = Exam()
+second_exam.writing_grade = 77
+second_exam.science_grade = 71
+second_exam.math_grade = 59
+print(f'Nauka -> ocena z drugiego egazminu: {second_exam.science_grade}')
+print(f'Nauka -> ocena z pierwszego egazminu: {first_exam.science_grade} -> błąd!!!')

e279b6182df7ab0a2838477725bb701d2d9c014a
albim72 <marcin.albiniak72@gmail.com>
2023-01-14 12:40:48 +0100

    Update main.py

--- DZIEN_5/BUCKET_PROJ/main.py
+++ DZIEN_5/BUCKET_PROJ/main.py
@@ -1,6 +1,21 @@
-from simplebucket import Bucket,fill
+from simplebucket import Bucket,fill,deduct
 
+print("______zbiornik danych nr 1_______")
 bucket = Bucket(60)
 print(bucket)
 fill(bucket,100)
 print(bucket)
+
+if deduct(bucket,99):
+    print('potrzebne 99 jednostek')
+else:
+    print('nie ma 99 jednostek')
+print(bucket)
+
+if deduct(bucket,3):
+    print('potrzebne 3 jednostek')
+else:
+    print('nie ma 3 jednostek')
+print(bucket)
+
+print("______zbiornik danych nr 2 [nowa wersja] _______")

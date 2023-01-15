72d75df2ab32533f093ecaf9fae3086db6d30430
albim72 <marcin.albiniak72@gmail.com>
2023-01-15 08:20:27 +0100

    Create sortowanie.py

---
+++ DZIEN_6/sortowanie.py
@@ -0,0 +1,5 @@
+nazwa_classic = ['samolot','mecz','miecz','Mieczysław','Konrad','wół',"Wisła","piorun","macierz","zenit","akronim"]
+
+
+
+nazwa_modern = ['samolot','mecz','miecz','Mieczysław','Konrad','wół',"Wisła","piorun","macierz","zenit","akronim"]

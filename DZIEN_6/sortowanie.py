1f80dee299170e8ea09a4fd3163a39b4ce68b336
albim72 <marcin.albiniak72@gmail.com>
2023-01-15 08:30:46 +0100

    Update sortowanie.py

--- DZIEN_6/sortowanie.py
+++ DZIEN_6/sortowanie.py
@@ -1,5 +1,26 @@
-nazwa_classic = ['samolot','mecz','miecz','Mieczysław','Konrad','wół',"Wisła","piorun","macierz","zenit","akronim"]
+nazwa_classic = ['samolot','mecz','miecz','Mieczysław','Konrad','wół',"Wisła","piorun","macierz","zenit","akronim","łódź","Łódź"]
 
+def bsort_classic(a):
+    for _ in range(len(a)):
+        for i in range(1,len(a)):
+            if a[i].lower() < a[i-1].lower():
+                temp = a[i]
+                a[i] = a[i-1]
+                a[i-1] = temp
+
+
+bsort_classic(nazwa_classic)
+print(nazwa_classic)
 
 
 nazwa_modern = ['samolot','mecz','miecz','Mieczysław','Konrad','wół',"Wisła","piorun","macierz","zenit","akronim"]
+
+def bsort_modern(a):
+    for _ in range(len(a)):
+        for i in range(1,len(a)):
+            if a[i].lower() < a[i-1].lower():
+                a[i-1],a[i] = a[i],a[i-1]
+
+
+bsort_modern(nazwa_modern)
+print(nazwa_modern)

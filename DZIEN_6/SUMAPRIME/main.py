88cbf6c4bff313ce3ed319734a78e1d70e7b18ae
albim72 <marcin.albiniak72@gmail.com>
2023-01-15 15:03:14 +0100

    Update main.py

--- DZIEN_6/SUMAPRIME/main.py
+++ DZIEN_6/SUMAPRIME/main.py
@@ -13,11 +13,22 @@ def synchroniczna():
     endtime = time.time()
 
     print(f'całkowity czas wyznaczenia sum - synchronicznie: {endtime - starttime} s')
-    
 
+def run_heavy_function(params):
+    return znajdz_sume_liczb_pierwszych(*params)
+
+def asynchronicznie():
+    starttime = time.time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
+        wynik = executor.map(run_heavy_function,numbers)
+    print(list(wynik))
+    endtime = time.time()
+
+    print(f'całkowity czas wyznaczenia sum - asynchronicznie: {endtime - starttime} s')
 
 def main():
     synchroniczna()
+    asynchronicznie()
 
 if __name__ == '__main__':
     main()

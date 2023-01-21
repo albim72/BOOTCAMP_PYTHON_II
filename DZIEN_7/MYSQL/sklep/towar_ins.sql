5d57e1a4cc460767433534fd4a8f5a7e90b844ca
albim72 <marcin.albiniak72@gmail.com>
2023-01-21 11:57:32 +0100

    Create towar_ins.sql

---
+++ DZIEN_7/MYSQL/sklep/towar_ins.sql
@@ -0,0 +1,4 @@
+INSERT INTO `sklep`.`towar` (`nazwa_towaru`, `data_prod`, `seria`) VALUES ('TV', '2021-10-10', 'T53545');
+INSERT INTO `sklep`.`towar` (`nazwa_towaru`, `data_prod`, `seria`) VALUES ('TV smart', '2022-09-18', 'sm23454543');
+INSERT INTO `sklep`.`towar` (`nazwa_towaru`, `data_prod`, `seria`) VALUES ('Radio T5', '2022-01-19', 'rv34');
+INSERT INTO `sklep`.`towar` (`nazwa_towaru`, `data_prod`, `seria`) VALUES ('Wieża', '2019-10-29', 'w35');

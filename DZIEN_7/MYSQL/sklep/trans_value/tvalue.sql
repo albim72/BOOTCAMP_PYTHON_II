3f8bc1d5f82c8f6e0bb95a34c63ecf5110e79791
albim72 <marcin.albiniak72@gmail.com>
2023-01-21 12:01:29 +0100

    Create tvalue.sql

---
+++ DZIEN_7/MYSQL/sklep/trans_value/tvalue.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sklep`.`transakcja` (`nr_faktury`, `idklienta`, `idtowaru`, `data_sprzed`, `sprzedawca`) VALUES ('434', '2', '1', '2023-01-10', 'Jan Nowak');
+INSERT INTO `sklep`.`transakcja` (`nr_faktury`, `idklienta`, `idtowaru`, `data_sprzed`, `sprzedawca`) VALUES ('455', '1', '3', '2023-01-12', 'Jan Nowak');
+INSERT INTO `sklep`.`transakcja` (`nr_faktury`, `idklienta`, `idtowaru`, `data_sprzed`, `sprzedawca`) VALUES ('478', '3', '4', '2023-01-14', 'Anna Poc');

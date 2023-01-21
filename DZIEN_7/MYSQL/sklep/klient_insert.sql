f1c40e0b0e9e467afce9acd15e7c114a992aea6b
albim72 <marcin.albiniak72@gmail.com>
2023-01-21 11:55:44 +0100

    Create klient_insert.sql

---
+++ DZIEN_7/MYSQL/sklep/klient_insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sklep`.`klient` (`imie`, `nazwisko`, `data_ur`, `telefon`) VALUES ('Jan', 'Kot', '1987-10-11', '766888999');
+INSERT INTO `sklep`.`klient` (`imie`, `nazwisko`, `data_ur`, `telefon`) VALUES ('Olga', 'Nowak', '1976-03-14', '555444333');
+INSERT INTO `sklep`.`klient` (`imie`, `nazwisko`, `data_ur`) VALUES ('Anna', 'Nowik', '1956-12-13');

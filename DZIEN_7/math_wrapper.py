e977c9c7f4e13c36ab74cb41c3b2d417eeebc6ff
albim72 <marcin.albiniak72@gmail.com>
2023-01-21 09:32:47 +0100

    Update math_wrapper.py

--- DZIEN_7/math_wrapper.py
+++ DZIEN_7/math_wrapper.py
@@ -1,7 +1,7 @@
 from functools import wraps
 
 def debug(func):
-    @wraps
+    @wraps(func)
     def wrapper(*args,**kwargs):
         print(f'Pełna nazwa metody: {func.__qualname__}')
         return func(*args,**kwargs)
@@ -13,3 +13,18 @@ def debugmethods(cls):
             setattr(cls,key,debug(val))
     return cls
 
+@debugmethods
+class Calc:
+    def add(self,x,y):
+        return x+y
+
+    def mul(self,x,y):
+        return x*y
+
+    def div(self,x,y):
+        return x/y
+
+mc = Calc()
+print(mc.add(4,7))
+print(mc.mul(6,7))
+print(mc.div(2,3))

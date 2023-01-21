c25b1ce56783278fa274e3683fb65f960e92a474
albim72 <marcin.albiniak72@gmail.com>
2023-01-21 09:48:34 +0100

    Update metawrapper.py

--- DZIEN_7/metawrapper.py
+++ DZIEN_7/metawrapper.py
@@ -3,25 +3,27 @@ from functools import wraps
 def debug(func):
     @wraps(func)
     def wrapper(*args,**kwargs):
-        print(f'Pełna nazwa metody: {func.__qualname__}')
+        print(f"pełna nazwa matody: {func.__qualname__}")
         return func(*args,**kwargs)
+
     return wrapper
 
+
 def debugmethods(cls):
+
     for key,val in vars(cls).items():
         if callable(val):
             setattr(cls,key,debug(val))
     return cls
 
-#metaklasa
-class debugMeta(type):
-    def __new__(cls, clsname,bases, attrs):
-        obj = super().__new__(cls, clsname,bases, attrs)
-        obj.debugmethods(obj)
+class DebugMeta(type):
+
+    def __new__(cls, clsname,bases,attrs):
+        obj = super().__new__(cls, clsname,bases,attrs)
+        obj = debugmethods(obj)
         return obj
 
-
-class Base(metaclass=debugMeta):pass
+class Base(metaclass=DebugMeta):pass
 
 class Calc(Base):
     def add(self,x,y):
@@ -33,7 +35,19 @@ class Calc(Base):
     def div(self,x,y):
         return x/y
 
+class Clc_dodaj(Base):
+    def add(self,x,y):
+        return x+y
+
+class Clc_multi(Clc_dodaj):
+    def mul(self,x,y):
+        return x*y
+
 mc = Calc()
 print(mc.add(4,7))
 print(mc.mul(6,7))
 print(mc.div(2,3))
+
+
+myc = Clc_multi()
+print(myc.mul(4,5))

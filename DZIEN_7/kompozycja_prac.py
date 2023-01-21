308e95d0f2d782d6948f7bf10aa98485d48a552b
albim72 <marcin.albiniak72@gmail.com>
2023-01-21 11:12:05 +0100

    Update kompozycja_prac.py

--- DZIEN_7/kompozycja_prac.py
+++ DZIEN_7/kompozycja_prac.py
@@ -1,21 +1,29 @@
 class Employee:
-    
+
     def __init__(self,name,age):
         self.name = name
         self.age = age
-        
+
     def emp_data(self):
         print(f'imię pracownika: {self.name}')
         print(f'wiek pracownika: {self.age}')
-        
-        
+
+
 class Data:
     def __init__(self,address,salary,emp_obj):
         self.address = address
         self.salary = salary
         self.emp_obj = emp_obj
-        
+
     def display(self):
         self.emp_obj.emp_data()
         print(f"adres zamieszkania pracownika: {self.address}")
         print(f"wyskość sprzdaży: {self.salary} zł")
+
+
+emp = Employee('Robert',37)
+data = Data('ul.Złota 6 Kraków',450900,emp)
+data.display()
+
+
+

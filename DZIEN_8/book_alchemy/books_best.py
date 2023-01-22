db5864493a3e588b3d8cc5cbcef3cce2f196dce6
albim72 <marcin.albiniak72@gmail.com>
2023-01-22 09:17:55 +0100

    Update books_best.py

--- DZIEN_8/book_alchemy/books_best.py
+++ DZIEN_8/book_alchemy/books_best.py
@@ -7,3 +7,15 @@ from sqlalchemy.sql import text
 
 
 print(sqlalchemy.__version__)
+
+metadata = MetaData()
+books = Table('books_best',metadata,
+              Column('id',Integer,primary_key=True),
+              Column('tytul',String(length=50)),
+              Column('autor',String(length=50)),
+              )
+
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://root:abc123@localhost:3306/bookstore',echo=True)
+s = sqlalchemy.inspect(engine).has_table("books_best")
+metadata.create_all(engine)
+print(s)

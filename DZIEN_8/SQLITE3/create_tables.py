b5e2fe13303c244833e24c91ba2aa000eb54941d
albim72 <marcin.albiniak72@gmail.com>
2023-01-22 10:47:30 +0100

    Create create_tables.py

---
+++ DZIEN_8/SQLITE3/create_tables.py
@@ -0,0 +1,11 @@
+import sqlite3
+from sqlite3 import Error
+from conn import create_connection
+
+def create_table(conn,create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+        

559058e507c0e4496c6d1255b6d306dd7e5e5ddc
albim72 <marcin.albiniak72@gmail.com>
2023-01-22 12:29:39 +0100

    Update select_data.py

--- DZIEN_8/SQLITE3/select_data.py
+++ DZIEN_8/SQLITE3/select_data.py
@@ -5,7 +5,7 @@ from conn import create_connection
 def select_all_tasks(conn):
     cur = conn.cursor()
     cur.execute("SELECT * FROM tasks")
-    
+
     rows = cur.fetchall()
     for row in rows:
         print(row)
@@ -18,3 +18,27 @@ def select_all_projects(conn):
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
+def select_task_by_priority(conn,priority):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks WHERE priority = ?",(priority,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+def select_taks():
+    database = r"c:\sqlite\db\bazaprojektow.db"
+    conn = create_connection(database)
+    with conn:
+        print("1. Wyświetlenie wszystkich zadań...")
+        select_all_tasks(conn)
+        print("2. Wyświetlenie wszystkich zadań po priorytecie...")
+        select_task_by_priority(conn,1)
+
+def select_projects():
+    database = r"c:\sqlite\db\bazaprojektow.db"
+    conn = create_connection(database)
+    with conn:
+        print("1. Wyświetlenie wszystkich projektów...")
+        select_all_projects(conn)

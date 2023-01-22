a0ac47abaa596952b7d7b819bcf1a2b1f2fa137b
albim72 <marcin.albiniak72@gmail.com>
2023-01-22 08:26:09 +0100

    Update users_alchemy.py

--- DZIEN_8/users_alchemy.py
+++ DZIEN_8/users_alchemy.py
@@ -4,3 +4,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = sqlalchemy.create_engine('mysql+mysqlconnector://root:abc123@localhost:3306/dbtestowa',echo=True)
+
+#metadefinicja i tworzenie tabeli
+Base = declarative_base()
+
+class User(Base):
+    
+    __tablename__ = 'users'
+    
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key = True)
+    name = sqlalchemy.Column(sqlalchemy.String(length=50))
+    fullname = sqlalchemy.Column(sqlalchemy.String(length=50))
+    nickname = sqlalchemy.Column(sqlalchemy.String(length=50))
+    
+    def __repr__(self):
+        return f"<USER(name = {self.name}, fullname = {self.fullname}, nickname = {self.nickname})>"

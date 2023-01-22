5fafa5ed7e72b5145ce8fe76b8127ea36fe1d932
albim72 <marcin.albiniak72@gmail.com>
2023-01-22 08:20:58 +0100

    Update users_alchemy.py

--- DZIEN_8/users_alchemy.py
+++ DZIEN_8/users_alchemy.py
@@ -2,3 +2,5 @@ import mysql.connector
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://root:abc123@localhost:3306/dbtestowa',echo=True)

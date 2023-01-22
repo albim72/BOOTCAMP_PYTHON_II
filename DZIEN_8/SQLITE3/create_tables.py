344e7550322a6a1b72038b41256a20b020841a27
albim72 <marcin.albiniak72@gmail.com>
2023-01-22 10:54:41 +0100

    Update create_tables.py

--- DZIEN_8/SQLITE3/create_tables.py
+++ DZIEN_8/SQLITE3/create_tables.py
@@ -8,4 +8,26 @@ def create_table(conn,create_table_sql):
         c.execute(create_table_sql)
     except Error as e:
         print(e)
-        
+
+def cr():
+    database = r"c:\sqlite\db\bazaprojektow.db"
+    sql_create_projects_table = """
+        CREATE TABLE IF NOT EXISTS projects (
+            id integer PRIMARY KEY,
+            name text NOT NULL,
+            begin_date text NOT NULL,
+            end_date text NOT NULL,
+    );
+    """
+    sql_create_tasks_table = """
+        CREATE TABLE IF NOT EXISTS tasks (
+            id integer NOT NULL PRIMARY KEY,
+            name text NOT NULL,
+            priority integer NOT NULL,
+            status_id integer NOT NULL,
+            project_id integer NOT NULL,
+            begin_date text NOT NULL,
+            end_date text NOT NULL,
+            FOREIGN KEY(project_id) REFERENCES projects(id)
+            );
+    """

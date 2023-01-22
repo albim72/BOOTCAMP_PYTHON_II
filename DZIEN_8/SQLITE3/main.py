31d9999e929a349a0565abdce316a3eb0e84c387
albim72 <marcin.albiniak72@gmail.com>
2023-01-22 12:34:00 +0100

    Update main.py

--- DZIEN_8/SQLITE3/main.py
+++ DZIEN_8/SQLITE3/main.py
@@ -2,7 +2,14 @@ import sqlite3
 from sqlite3 import Error
 from conn import create_connection
 from create_tables import cr
+from insert_data import insert
+from select_data import select_taks, select_projects
+
 
 if __name__ == '__main__':
     #create_connection(r"c:\sqlite\db\bazaprojektow.db")
-    cr()
+    #cr()
+    insert()
+    select_taks()
+    print("__________________")
+    select_projects()

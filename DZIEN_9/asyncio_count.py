d4679193e14287bbdc18b34aeea54c908c8b688f
albim72 <marcin.albiniak72@gmail.com>
2023-01-28 14:05:27 +0100

    Update asyncio_count.py

--- DZIEN_9/asyncio_count.py
+++ DZIEN_9/asyncio_count.py
@@ -1,4 +1,5 @@
 import time
+import asyncio
 
 def count():
     print("Jeden")
@@ -8,12 +9,23 @@ def count():
 def main():
     for _ in range(3):
         count()
-        
-        
+
+async def count_async():
+    print("Jeden")
+    await asyncio.sleep(1)
+    print("Dwa")
+
+async def main_spec():
+    await asyncio.gather(count_async(),count_async(),count_async())
 
 
 if __name__ == '__main__':
     s = time.perf_counter()
     main()
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} wykonany w {elapsed:0.2f} s.")
+
+    s = time.perf_counter()
+    asyncio.run(main_spec())
     elapsed = time.perf_counter() - s
     print(f"{__file__} wykonany w {elapsed:0.2f} s.")

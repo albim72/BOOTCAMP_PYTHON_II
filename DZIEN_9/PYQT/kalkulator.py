1c51844f3330a523869d270f26748f732f494f4a
albim72 <marcin.albiniak72@gmail.com>
2023-01-28 15:51:45 +0100

    Update kalkulator.py

--- DZIEN_9/PYQT/kalkulator.py
+++ DZIEN_9/PYQT/kalkulator.py
@@ -15,12 +15,43 @@ class Kalkulator(QWidget):
         etykieta2 = QLabel("Liczba 2:",self)
         etykieta3 = QLabel("Wynik:",self)
 
+        #pola edycyjne
+
+        self.liczba1Edt = QLineEdit()
+        self.liczba2Edt = QLineEdit()
+        self.wynikEdt = QLineEdit()
+
+        self.wynikEdt.readonly = True;
+        self.wynikEdt.setToolTip('Wpisz <b>liczby</b> i wybierz działanie: ')
+
         #przypisanie widgetów do układu tabelarycznego
 
         ukladT = QGridLayout()
         ukladT.addWidget(etykieta1,0,0)
         ukladT.addWidget(etykieta2,0,1)
         ukladT.addWidget(etykieta3,0,2)
+        ukladT.addWidget(self.liczba1Edt,1,0)
+        ukladT.addWidget(self.liczba2Edt,1,1)
+        ukladT.addWidget(self.wynikEdt,1,2)
+
+
+        #przyciski
+        dodajBtn = QPushButton("&Dodaj",self)
+        odejmijBtn = QPushButton("&Odejmij",self)
+        dzielBtn = QPushButton("&Dziel",self)
+        mnozBtn = QPushButton("&Mnóż",self)
+        koniecBtn = QPushButton("&Koniec",self)
+        koniecBtn.resize(koniecBtn.sizeHint())
+
+        ukladH = QHBoxLayout()
+        ukladH.addWidget(dodajBtn)
+        ukladH.addWidget(odejmijBtn)
+        ukladH.addWidget(dzielBtn)
+        ukladH.addWidget(mnozBtn)
+
+        ukladT.addLayout(ukladH,2,0,1,3)
+        ukladT.addWidget(koniecBtn,3,0,1,3)
+
 
         #przypięcie layoutu do okna
         self.setLayout(ukladT)

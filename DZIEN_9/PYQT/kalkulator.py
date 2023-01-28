91173c6bb05b974163d9dc9c0a0bcb8e71856211
albim72 <marcin.albiniak72@gmail.com>
2023-01-28 15:09:36 +0100

    Create kalkulator.py

---
+++ DZIEN_9/PYQT/kalkulator.py
@@ -0,0 +1,35 @@
+from PyQt5.QtWidgets import QApplication,QWidget, QLabel, QGridLayout,QLineEdit,QPushButton,QHBoxLayout
+from PyQt5.QtGui import QIcon
+import sys
+
+
+class Kalkulator(QWidget):
+    def __init__(self,parent=None):
+        super().__init__(parent)
+        self.interfejs()
+
+    def interfejs(self):
+
+        #etykiety
+        etykieta1 = QLabel("Liczba 1:",self)
+        etykieta2 = QLabel("Liczba 2:",self)
+        etykieta3 = QLabel("Wynik:",self)
+
+        #przypisanie widgetów do układu tabelarycznego
+
+        ukladT = QGridLayout()
+        ukladT.addWidget(etykieta1,0,0)
+        ukladT.addWidget(etykieta2,0,1)
+        ukladT.addWidget(etykieta3,0,2)
+
+        #przypięcie layoutu do okna
+        self.setLayout(ukladT)
+        #self.resize(300,100)
+        self.setGeometry(20,20,300,100)
+        self.setWindowTitle("Prosty kalkulator")
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    okno = Kalkulator()
+    sys.exit(app.exec_())

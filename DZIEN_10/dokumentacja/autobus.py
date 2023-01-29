b49c20ca6342b2c737559d1e2393d2cc552e3ec9
albim72 <marcin.albiniak72@gmail.com>
2023-01-29 15:01:12 +0100

    Update autobus.py

--- DZIEN_10/dokumentacja/autobus.py
+++ DZIEN_10/dokumentacja/autobus.py
@@ -1,3 +1,5 @@
+#opis aplikacji
+
 """
 Prosta aplikacja do przypisywania miejsc w autobusie przy zakupie biletu
 wersja nr A.1.1
@@ -7,3 +9,5 @@ wersja nr A.1.1
 """
 to jest drugi komentarz dla aplikacji Autobus
 """
+
+#koniec opisu

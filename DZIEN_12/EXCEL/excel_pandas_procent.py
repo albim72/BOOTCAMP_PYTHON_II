d4161135252f4f9a1b86d8616572ae7705db9af0
albim72 <marcin.albiniak72@gmail.com>
2023-02-12 15:30:27 +0100

    Update excel_pandas_procent.py

--- DZIEN_12/EXCEL/excel_pandas_procent.py
+++ DZIEN_12/EXCEL/excel_pandas_procent.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import xlsxwriter
+import openpyxl
 
 
 df = pd.DataFrame({
@@ -7,3 +8,22 @@ df = pd.DataFrame({
     'Wartość sprzedaży':[2340,2189,8970,5460,3425,7890,1234,2567,6789,5432,9854,6782],
     'Procent':[.04,.08,.11,.16,.28,.18,.24,.31,.09,.1,.4,.14]
 })
+
+writer = pd.ExcelWriter('procenty.xlsx', engine='xlsxwriter')
+df.to_excel(writer,sheet_name='proc')
+
+workbook = writer.book
+worksheet = writer.sheets['proc']
+
+format1 = workbook.add_format({'num_format':'#.##00'})
+format2 = workbook.add_format({'num_format':'0.0%'})
+
+worksheet.set_column(2,2,20,format1)
+worksheet.set_column(3,3,None,format2)
+
+writer.save()
+
+df2 = pd.read_excel('procenty.xlsx',sheet_name='proc')
+# pd.read_excel(r'C:\BOOTCAMP_PYTHON_II_22_23\excel_pandas_procent\procenty.xlsx')
+
+print(df2)

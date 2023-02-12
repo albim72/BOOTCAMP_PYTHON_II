29c8bf561fb0b784ecf169cbdd298f83f0ed3185
albim72 <marcin.albiniak72@gmail.com>
2023-02-12 08:15:58 +0100

    Create marki_wykres.py

---
+++ DZIEN_12/marki_wykres.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+fig,ax = plt.subplots()
+
+marki = ['audi','toyota','mercedes','honda','kia','jeep','seat']
+sprzed = [12.7,28.9,9.8,16.8,23.5,7.8,28.3]
+
+bar_labels = ['audi','toyota','mercedes','honda','kia','jeep','seat']
+bar_colors = ['tab:red','tab:green','tab:','tab:','tab:orange','tab:red','tab:black']

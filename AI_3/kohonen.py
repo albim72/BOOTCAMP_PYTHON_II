7350c5c13506e4d9cc5d7017cbe855bb9933ead7
albim72 <marcin.albiniak72@gmail.com>
2023-05-27 13:42:37 +0200

    Update kohonen.py

--- AI_3/kohonen.py
+++ AI_3/kohonen.py
@@ -26,4 +26,68 @@ for pl in ds['petal length']:
 for pw in ds['petal width']:
     pw = (pw - min(ds['petal width'])) / (max(ds['petal width'] - min(ds['petal width'])))
     list_pw.append(pw)
-    
+
+X = np.array(list(zip(list_sl,list_sw,list_pl,list_pw)))
+
+nc = 3 #liczba klas (podgatunki Irysa)
+W=[] #lista wektorów w (wektory mapy, pomiędzy sąsiadami)
+M = len(X) #liczba wektorów x
+N = len(X[0]) #wymiar wektora x
+
+def get_weights():
+    y = np.random.random()*(2.0/np.sqrt(M))
+    return 0.5-(1/np.sqrt(M))+y
+
+
+for i in range(nc):
+    W.append(list())
+    for j in range(N):
+        W[i].append(get_weights()*0.5)
+
+def distance(w,x):
+    r=0,
+    for i in range(len(W)):
+        r = r+(w[i]-x[i])**2
+    r = np.sqrt(r)
+    return r
+
+def findclosest(W,x):
+    wm = W[0]
+    r = distance(wm,x)
+    i=0
+    i_n = i
+    for w in W:
+        if distance(w,x)<r:
+            r = distance(w,x)
+            wm = w
+            i_n = i
+        i = i+1
+print(W)
+
+la =  0.3 #lambda
+dla = 0.05 #delta lambda
+
+while la >=0:
+    for k in range(10):
+        for x in X:
+            wm = findclosest(W,x)[0]
+            for i in range(len(wm)):
+                wm[i] = wm[i] + la*(x(i)-wm[i])
+    la = la-dla
+
+Data = list()
+
+for i in range(len(W)):
+    Data.append(list())
+
+dfList = ds['class'].to_numpy()
+
+DS = list()
+
+for x in X:
+    i_n = findclosest(W,x)[1]
+    Data[i_n].append(x)
+    DS.append(([i_n],dfList[i]))
+    i = i+1
+print(DS)
+

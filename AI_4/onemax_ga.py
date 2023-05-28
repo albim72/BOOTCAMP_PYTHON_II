f2094df43e71e1184abf5a7aa560b34866ea95a7
albim72 <marcin.albiniak72@gmail.com>
2023-05-28 13:58:43 +0200

    Update onemax_ga.py

--- AI_4/onemax_ga.py
+++ AI_4/onemax_ga.py
@@ -31,3 +31,44 @@ toolbox.register("mutate",tools.mutFlipBit,indpb = 0.05)
 
 #operator selekcji
 toolbox.register("select",tools.selTournament,tournsize=3)
+
+#proces ewolucji
+
+def main():
+    random.seed(64)
+    pop = toolbox.population(n=300)
+    CXPB, MUTPB = 0.5,0.2
+    print("start procesu ewolucji...")
+    #ewaluacja wejściowe populacji
+    fitnesses = list(map(toolbox.evalute,pop))
+    for ind,fit in zip(pop,fitnesses):
+        ind.fitness.values = fit
+    
+    print(f"przeprowadzono ewaluację {len(pop)} osobników")
+    
+    fits = [ind.fitness.values[0] for ind in pop]
+    
+    #numer generacji g
+    g=0
+    
+    #Ewolucja
+    while max(fits) < 100 and g < 1000:
+        g = g+1
+        print(f"---- Generacja {g} ------")
+        offspring = toolbox.select(pop,len(pop))
+        offspring = list(map(toolbox.clone,offspring))
+        
+        #zastosowanie krzyżowania i mutacji do modyfikacji nowej populacji
+        for child1, child2 in zip(offspring[::2],offspring[1::2]):
+            if random.random() < CXPB:
+                toolbox.mate(child1,child2)
+                del child1.fitness.values
+                del child2.fitness.values
+                
+        for mutant in offspring:
+            if random.random < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+                
+                
+        
